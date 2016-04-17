61b6ce37260e3fb3ba6d8749e32734920963846a
Kevin W Monroe <kevin.monroe@canonical.com>
2016-04-16 22:59:52 -0600

    use facter to get fqdn; use roles vs components during install

--- reactive/namenode.py
+++ reactive/namenode.py
@@ -3,15 +3,19 @@ from charms.reactive import when_not
 from charms.reactive import set_state
 from charms.layer.apache_bigtop_base import get_bigtop_base, get_layer_opts
 from charmhelpers.core import hookenv
+from jujubigdata import utils
 import subprocess
 
 
+@when('puppet.available')
 @when_not('namenode.installed')
 def install_hadoop():
     hookenv.status_set('maintenance', 'installing namenode')
     bigtop = get_bigtop_base()
-    nn_fqdn = subprocess.check_output(['hostname', '-f']).strip().decode()
-    bigtop.install(NN=nn_fqdn)
+    # nn_host = utils.resolve_private_address(hookenv.unit_private_ip())
+    nn_host = subprocess.check_output(['facter', 'fqdn']).strip().decode()
+    hosts = {'namenode': nn_host}
+    bigtop.install(hosts=hosts, roles='namenode')
     set_state('namenode.installed')
     hookenv.status_set('maintenance', 'namenode installed')
 
@@ -29,15 +33,17 @@ def start_namenode():
 @when('datanode.joined')
 def send_fqdn_to_dn(datanode):
     '''Send datanodes our FQDN so they can install as slaves.'''
-    hostname = subprocess.check_output(['hostname', '-f']).strip().decode()
-    datanode.send_namenodes([hostname])
+    # nn_host = utils.resolve_private_address(hookenv.unit_private_ip())
+    nn_host = subprocess.check_output(['facter', 'fqdn']).strip().decode()
+    datanode.send_namenodes([nn_host])
 
 
 @when('namenode.clients')
 def send_fqdn_to_client(client):
     '''Send clients our FQDN so they can install as slaves.'''
-    hostname = subprocess.check_output(['hostname', '-f']).strip().decode()
-    client.send_namenodes([hostname])
+    # nn_host = utils.resolve_private_address(hookenv.unit_private_ip())
+    nn_host = subprocess.check_output(['facter', 'fqdn']).strip().decode()
+    client.send_namenodes([nn_host])
 
 
 @when('benchmark.joined')
