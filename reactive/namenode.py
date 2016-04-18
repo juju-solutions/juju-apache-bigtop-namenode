a28dac603d8daddfed3d4c7456a4f2af570a63db
Kevin W Monroe <kevin.monroe@canonical.com>
2016-04-18 20:41:27 +0000

    start services (even though install should do it, lets be explicit); add datanode count to status

--- reactive/namenode.py
+++ reactive/namenode.py
@@ -2,7 +2,7 @@ from charms.reactive import when
 from charms.reactive import when_not
 from charms.reactive import set_state
 from charms.layer.apache_bigtop_base import get_bigtop_base, get_layer_opts
-from charmhelpers.core import hookenv
+from charmhelpers.core import hookenv, host
 from jujubigdata import utils
 import subprocess
 
@@ -24,6 +24,9 @@ def install_hadoop():
 @when_not('namenode.started')
 def start_namenode():
     hookenv.status_set('maintenance', 'starting namenode')
+    # NB: service should be started by install, but this may be handy in case
+    # we have something that removes the .started state in the future.
+    host.service_start('hadoop-hdfs-namenode')
     for port in get_layer_opts().exposed_ports('namenode'):
         hookenv.open_port(port)
     set_state('namenode.started')
@@ -36,6 +39,14 @@ def send_fqdn_to_dn(datanode):
     # nn_host = utils.resolve_private_address(hookenv.unit_private_ip())
     nn_host = subprocess.check_output(['facter', 'fqdn']).strip().decode()
     datanode.send_namenodes([nn_host])
+
+    # update status with slave count and report ready for hdfs
+    slaves = [node['host'] for node in datanode.nodes()]
+    hookenv.status_set('active', 'ready ({count} datanode{s})'.format(
+        count=len(slaves),
+        s='s' if len(slaves) > 1 else '',
+    ))
+    set_state('namenode.ready')
 
 
 @when('namenode.clients')
